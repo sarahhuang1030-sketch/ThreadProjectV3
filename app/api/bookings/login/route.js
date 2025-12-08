cae05e3f644f215d3e2f389703b8c55e35b6372d
sarahhuang1030 <sarahhuang1030@gmail.com>
2025-12-08 08:39:51 -0700

    fix route.js

--- app/api/bookings/login/route.js
+++ app/api/bookings/login/route.js
@@ -1,19 +1,30 @@
 // app/api/login/route.js
 import { NextResponse } from "next/server";
-import pool from "../../../lib/database"; // if you're inside app/api/login/route.js
+import {getPool} from "../../../lib/database"; // if you're inside app/api/login/route.js
 
 //import mysql from "mysql2/promise";
 export async function POST(req) {
   try {
+    const pool = await getPool();
     const body = await req.json();
     const { CustFirstName, CustLastName, CustEmail } = body;
 
-    const [rows] = await pool.execute(
-      "SELECT * FROM customers WHERE CustFirstName = ? AND CustLastName = ? AND CustEmail = ?",
-      [CustFirstName, CustLastName, CustEmail]
-    );
+    const result = await pool
+      .request()
+      .input("CustFirstName", CustFirstName)
+      .input("CustLastName", CustLastName)
+      .input("CustEmail", CustEmail)
+      .query(`
+        SELECT *
+        FROM customers
+        WHERE CustFirstName = @CustFirstName
+          AND CustLastName = @CustLastName
+          AND CustEmail = @CustEmail
+      `);
 
-    const success = Array.isArray(rows) && rows.length > 0;
+    const rows = result.recordset;
+    const success = rows.length > 0;
+    
     if (success) {
       return NextResponse.json({ success: success, user: rows[0] });
     } else {
